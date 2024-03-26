ca0c8a150f121fd0b562661c0bae15f91a1af5dd
KarelMoricky <KarelMoricky@users.noreply.github.com>
2024-03-26 23:04:25 +0100

    Changed: Credits use Robot Slab

--- scripts/localization.js
+++ scripts/localization.js
@@ -148,8 +148,8 @@ var Localization = new function()
         },
         {
             id: "credits",
-            "en": "Game by <a href=\"https://www.instagram.com/dododollydo/\">Mom</a> and <a href=\"http://moricky.com\">Dad</a>, art by <a href=\"https://www.behance.net/mikovecjir5bc4\">Jirka</a>, audio by <a href=\"#\">TBD</a>, testing by <a href=\"https://twitter.com/YorisYan\">Joris</a>, <a href=\"https://www.instagram.com/katacik/\">Katka</a>, and <a href=\"https://twitter.com/maruksp\">Marek</a>",
-            "cs": "Hra od <a href=\"https://www.instagram.com/dododollydo/\">maminky</a> a <a href=\"http://moricky.com\">tatínka</a>, grafika od <a href=\"https://www.behance.net/mikovecjir5bc4\">Jirky</a>, zvuky od <a href=\"#\">TBD</a>, testovali <a href=\"https://twitter.com/YorisYan\">Joris</a>, <a href=\"https://www.instagram.com/katacik/\">Katka</a>, a <a href=\"https://twitter.com/maruksp\">Marek</a>",
+            "en": "Game by <a href=\"https://www.instagram.com/dododollydo/\">Mom</a> and <a href=\"http://moricky.com\">Dad</a>, art&nbsp;by&nbsp;<a href=\"https://www.behance.net/mikovecjir5bc4\">Jirka</a>, audio&nbsp;by&nbsp;<a href=\"#\">TBD</a>, testing&nbsp;by&nbsp;<a href=\"https://twitter.com/YorisYan\">Joris</a>, <a href=\"https://www.instagram.com/katacik/\">Katka</a>, and&nbsp;<a href=\"https://twitter.com/maruksp\">Marek</a>",
+            "cs": "Hra od <a href=\"https://www.instagram.com/dododollydo/\">maminky</a> a <a href=\"http://moricky.com\">tatínka</a>, grafika&nbsp;od&nbsp;<a href=\"https://www.behance.net/mikovecjir5bc4\">Jirky</a>, zvuky&nbsp;od&nbsp;<a href=\"#\">TBD</a>, testovali&nbsp;<a href=\"https://twitter.com/YorisYan\">Joris</a>, <a href=\"https://www.instagram.com/katacik/\">Katka</a>, a&nbsp;<a href=\"https://twitter.com/maruksp\">Marek</a>",
             "ru": "",
         },
         //#endregion
