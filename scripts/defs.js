69108fb17bf8d72450252bd92c062d78a84bd53a
KarelMoricky <KarelMoricky@users.noreply.github.com>
2024-03-28 22:55:16 +0100

    Changed: Tile starting positions

--- scripts/defs.js
+++ scripts/defs.js
@@ -23,15 +23,19 @@ const TARGET_POSITIONS = new Map([
 ]);
 
 //--- Starting positions of individual tiles. Imported from Google Sheets.
-const ORIGIN_POSITIONS = new Map([								
-    ["tile06", [-4,-2]],		
-    ["tile08", [-3,-3]],	["tile09", [-3,-4]],
-["tile02", [-2,-2]],	["tile05", [-2,-3]],	
-        
-        
-["tile07", [1,4]],	["tile10", [1,3]],			["tile01", [1,0]],				
-["tile03", [2,3]],							
-["tile12", [3,3]],	["tile04", [3,2]],	["tile11", [3,1]],					
+const ORIGIN_POSITIONS = new Map([	
+    ["tile01", [1,0]],
+    ["tile02", [-2,-2]],
+    ["tile03", [2,3]],
+    ["tile04", [3,2]],
+    ["tile05", [-2,-3]],
+    ["tile06", [-4,-2]],
+    ["tile07", [2,3]],
+    ["tile08", [3,1]],
+    ["tile09", [-2,-2]],
+    ["tile10", [3,1]],
+    ["tile11", [3,0]],
+    ["tile12", [3,3]],
 ]);
 
 const ISO_SIZE = 140;
